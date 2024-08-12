e6c8b7164ceda504f38d2b90b17c04d5d17c78f8
Jackson Reynolds <jwreynold@gmail.com>
2024-08-12 11:10:33 -0400

    init day 7

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,10 +1,13 @@
 import fs from "fs";
 
-export const readLines = (fileName: string | number) => {
+export const readLines = <T = any>(
+  fileName: string | number,
+  mapFn: (e: string) => T = (e: string) => e as T,
+): T[] => {
   try {
     const filePath = `src/2023/inputs/${fileName}.txt`;
     const fileContent = fs.readFileSync(filePath, "utf-8");
-    return fileContent.split("\n");
+    return fileContent.split("\n").map(mapFn);
   } catch (e) {
     console.log("Oopsie file read error:", e);
     throw e;
